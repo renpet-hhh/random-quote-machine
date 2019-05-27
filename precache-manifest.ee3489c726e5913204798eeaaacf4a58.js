b3e2721c7b9028f24204929d05af8776b7c5f988
Renpet-hhh <renandcsc@gmail.com>
2019-05-27 16:44:48 -0300

    Updates

---
+++ precache-manifest.ee3489c726e5913204798eeaaacf4a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990c05ddef5a6c58944ec1b397cdc4bc",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "8edb9f4161517ccd28df",
+    "url": "/random-quote-machine/static/css/main.f67a71a3.chunk.css"
+  },
+  {
+    "revision": "ad90a80514bf06c3216f",
+    "url": "/random-quote-machine/static/js/2.1f44fb12.chunk.js"
+  },
+  {
+    "revision": "8edb9f4161517ccd28df",
+    "url": "/random-quote-machine/static/js/main.fe70a136.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
